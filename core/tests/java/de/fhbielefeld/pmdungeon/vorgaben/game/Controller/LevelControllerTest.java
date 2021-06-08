43b36d625795754fad5b24f7da6eaf5aeefc0998
NicolaKuehnert <nicola.kuehnert@outlook.de>
2021-06-08 14:49:22 +0200

    pom poms

---
+++ core/tests/java/de/fhbielefeld/pmdungeon/vorgaben/game/Controller/LevelControllerTest.java
@@ -0,0 +1,38 @@
+package de.fhbielefeld.pmdungeon.vorgaben.game.Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LevelControllerTest {
+
+    /**TODO
+     * Create LevelInstance
+     * Create MainController?
+     * **/
+
+    @Test
+    void loadDungeon() {
+
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void checkForTrigger() {
+    }
+
+    @Test
+    void triggerNextStage() {
+    }
+
+    @Test
+    void getDungeon() {
+    }
+
+    @Test
+    void draw() {
+    }
+}
---
+++ core/tests/java/de/fhbielefeld/pmdungeon/vorgaben/tools/PointTest.java
@@ -0,0 +1,7 @@
+package de.fhbielefeld.pmdungeon.vorgaben.tools;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+}
