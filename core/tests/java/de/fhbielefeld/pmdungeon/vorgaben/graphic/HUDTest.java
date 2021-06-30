30c4c926b926fe1203300d53aea6f1bb5698cb68
d88226d304dc0fa77644914a0988c72dfb374bf3 <alexander.del@fh-bielefeld.de>
2021-06-30 08:10:36 +0200

    added graphic-package and DungeonCamera Tests

---
+++ core/tests/java/de/fhbielefeld/pmdungeon/vorgaben/graphic/HUDTest.java
@@ -0,0 +1,86 @@
+package de.fhbielefeld.pmdungeon.vorgaben.graphic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+import de.fhbielefeld.pmdungeon.vorgaben.interfaces.IHUDElement;
+
+class HUDTest {
+
+    @Test 
+    void createHUDObject() {
+        HUD hud = new HUD();
+        assertNotNull(hud);
+    }
+
+    @Test 
+    void usePixelSystemThrowsNoException() {
+        HUD hud = new HUD();
+        hud.usePixelSystem(true);
+        hud.usePixelSystem(false);
+    }
+
+    @Test 
+    void addHudElementThrowsNoException() {
+        HUD hud = new HUD();
+        IHUDElement element = mock(IHUDElement.class);
+        hud.addHudElement(element);
+    }
+
+    @Test 
+    void removeHudElementThrowsNoException() {
+        HUD hud = new HUD();
+        IHUDElement element = mock(IHUDElement.class);
+        hud.addHudElement(element);
+
+        hud.removeHudElement(element);
+    }
+
+    @Test 
+    void removeHudElementThrowsNoExceptionWithoutAddedObject() {
+        HUD hud = new HUD();
+
+        hud.removeHudElement(element);
+    }
+
+    @Test 
+    void drawThrowsNoException() {
+        HUD hud = new HUD();
+
+        IHUDElement element = mock(IHUDElement.class);        
+        Texture texture = mock(Texture.class);
+        Point p = new Point(20,20);
+
+        when(element.getTexture()).thenReturn(texture);
+        when(element.getWidth()).thenReturn(4);
+        when(element.getHeight()).thenReturn(4);
+        when(element.getPosition()).thenReturn(p);
+
+        when(texture.getWidth()).thenReturn(4);
+
+        hud.usePixelSystem(false);
+        hud.draw();
+        hud.usePixelSystem(true);
+        hud.draw();
+    }
+
+    @Test 
+    void drawThrowsNoExceptionWithNoHUDElements() {
+        HUD hud = new HUD();
+        hud.usePixelSystem(false);
+        hud.draw();
+        hud.usePixelSystem(true);
+        hud.draw();
+    }
+
+    @Test 
+    void resizeThrowsNoException() {
+        HUD hud = new HUD();
+        hud.usePixelSystem(true);
+        hud.resize();
+        hud.usePixelSystem(false);
+        hud.resize();
+    }
+
+}
---
+++ core/tests/java/de/fhbielefeld/pmdungeon/vorgaben/graphic/TextStageTest.java
@@ -0,0 +1,68 @@
+package de.fhbielefeld.pmdungeon.vorgaben.graphic;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+
+class TextStageTest {
+
+    @Test 
+    void createTextStage() {
+        SpriteBatch sb = new SpriteBatch();
+
+        TextStage ts = new TextStage(sb);
+    }
+
+    @Test 
+    void drawText() {
+        SpriteBatch sb = new SpriteBatch();
+        TextStage ts = new TextStage(sb);
+
+        String text = "Test";
+        String fontPath = "assets/Asdonuts.ttf";
+        Color color = Color.RED;
+        int size = 30;
+        int width = 50;
+        int height = 50;
+        int x = 30;
+        int y = 400;
+
+        Label label = ts.drawText(text, fontPath, color, size, width, height, x, y);
+
+        assertEquals(label.getText(), text);
+    }
+
+    @Test 
+    void removeText() {
+        SpriteBatch sb = new SpriteBatch();
+        TextStage ts = new TextStage(sb);
+
+        String text = "Test";
+        String fontPath = "assets/Asdonuts.ttf";
+        Color color = Color.RED;
+        int size = 30;
+        int width = 50;
+        int height = 50;
+        int x = 30;
+        int y = 400;
+
+        Label label = ts.drawText(text, fontPath, color, size, width, height, x, y);
+
+        ts.removeText(label);
+    }
+
+    @Test 
+    void removeTextWithNoAddedText() {
+        SpriteBatch sb = new SpriteBatch();
+        TextStage ts = new TextStage(sb);
+
+        Label label = mock(Label.class);
+        ts.removeText(label);
+    }
+}
